859cd27aef25b781eedd90aef79ae237267cadee
notosgnikusnsi <notosgnikusnsi@gmail.com>
2023-08-21 23:14:45 +0900

    refactor:MongoDB接続 urlの記述方法の変更、connectにtryを使用

--- server.ts
+++ server.ts
@@ -1,13 +1,29 @@
 import { Bson, MongoClient } from "https://deno.land/x/mongo@v0.31.0/mod.ts";
 import "https://deno.land/std@0.193.0/dotenv/load.ts"
 
-const url = Deno.env.get("MONGO_URL_2");
+const user = Deno.env.get("USER")
+const password = Deno.env.get("PASSWORD")
+const dbClusterName = Deno.env.get("DB_CLUSTER_NAME")
+const dbName = Deno.env.get("DB_NAME")
+// const url = Deno.env.get("MONGO_URL_2");
+const url = `mongodb+srv://${user}:${password}@${dbClusterName}/${dbName}?authMechanism=SCRAM-SHA-1`;
+
 console.log(url)
 
 const client = new MongoClient();
 
-await client.connect(url);
-console.log("success!!")
+const run = async() => {
+  try {
+    await client.connect(url);
+    console.log("You successfully connected to MongoDB!")
+  } finally {
+    await client.close();
+  }
+} 
+
+run().catch(console.dir);
+
+/*
 
 interface UserSchema {
   _id: ObjectId;
@@ -33,3 +49,5 @@ const insertIds = await users.insertMany([
     password: "pass2",
   },
 ]);
+
+*/
